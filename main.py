24d57a4ac2665571e3aebe540d54c7bb7122f602
Dexter Mack <Dextermack111@gmail.com>
2024-04-11 14:04:58 -0700

    I used git to commit this, lol look at my new skills

---
+++ main.py
@@ -0,0 +1,13 @@
+# As of April 11, 2024 I've been coding python for about a year on and off, but I wanted to start over
+# and stay consistent for a long time so I am starting from the beginning
+# print("Hello World!")
+
+print("Hello my name is Dexter Mack, I am a first-year statistics student at Santa Monica College learning python")
+
+# This is string concatenation
+# String is a simple data structure
+print("Hello" + " " + "Dexter")
+
+name = input("What is your name? ")
+print(f"Hello {name}")
+
